0d3ddda0205d349e7cc49c3e4802fc22e4014586
GermanRodrickson <germanrodrickson@gmail.com>
2019-01-23 06:29:32 +0100

    css nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -12,12 +12,38 @@ const Wrapper = styled.section`
   padding: 40px 0px;
 `;
 
+const Nav = styled.ul`
+  display: flex;
+  flex-direction: row;
+
+  li {
+    margin: 0px 20px;
+    padding-bottom: 10px;
+    transition: all 0.2s linear;
+    background: (linear, left top, right top, from(#ffffff), to(#ffffff));
+    background: linear-gradient(to right, #ffffff 0%, #ffffff 100%);
+    background-size: 0 4px;
+    background-repeat: no-repeat;
+    background-position: left 110%;
+  }
+
+  li:hover {
+    background-size: 100% 4px;
+    color: #ffffff;
+  }
+`;
+
 class Prueba extends Component {
   render() {
     return (
       <Wrapper>
-        <div>logo</div>
-        <div>nav</div>
+        <h1>logo</h1>
+        <Nav>
+          <li>Home</li>
+          <li>Shop</li>
+          <li>More</li>
+          <li>Info</li>
+        </Nav>
         <div>cart</div>
       </Wrapper>
     );
